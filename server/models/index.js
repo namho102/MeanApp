3c6e6ff04eb00df0414a4b9ac3bfe37bfbb64ca5
Hoang Nam <namhoag.td09@gmail.com>
2015-08-31 09:32:44 +0700

    Initial commit

---
+++ server/models/index.js
@@ -0,0 +1,7 @@
+
+module.exports = {
+  movie: require('./Movie')
+};
+
+// A good developer almost always has opinions not just about his own code, but
+// about the whole direction of the project.
